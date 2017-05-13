4a1c5d071237c1b4e382135ac42f77221024adc2
Filip Messa <messa.filip@gmail.com>
2017-05-13 11:30:58 +0200

    init commit

---
+++ fibonator.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function coroutine(g) {
+  const it = g();
+  return argument => it.next(argument);
+}
+
+function* fibonator() {
+  let current = 0, next = 1;
+  while (true) {
+    yield current;
+    [current, next] = [next, current + next];
+  }
+}
+
+const run = coroutine(fibonator);
+
+for (let i = 0; i < 15; i++) {
+  console.log(run().value);
+}
